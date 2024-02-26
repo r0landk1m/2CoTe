5169e849b736a92c890a070cf406c12ad95759d8
DeaSeong <143526694+r0landk1m@users.noreply.github.com>
2024-02-26 14:29:04 +0000

    02.26

---
+++ Implementation/5430.py
@@ -0,0 +1,18 @@
+test_case = int(input())
+
+for _ in range(test_case):
+  case = list(input())
+  count_R = case.count("R")
+  count_D = case.count("D")
+  count_R %= 2
+  n = int(input())
+  str = input()
+  str = str[1:len(str) - 1]
+  array = str.split(",")
+
+  if count_D <= n - 1:
+    if count_R == 1:
+      array.reverse()
+    print(array[count_D:])
+  else:
+    print("error")
